73e95f9f67398749d457bf6ccef320853982bb61
GokulKrishnan10 <gokulkrish.elangovan10@gmail.com>
2022-12-11 20:48:19 +0530

    Added validity for paranthesis

--- calculator.js
+++ calculator.js
@@ -6,9 +6,17 @@ function Click(event) {
   // if (["*", "/", "+", "-"].includes(event.target.value)) {
   //   operator = event.target.value;
   // }
-  if (!event.target.value) {
+  const target = event.target.value;
+  if (!target) {
     console.log("Clicked Clear");
-    display.innerText = "0";
+    display.innerText = "";
+    return;
+  }
+  if (target === "=") {
+    //Checking for validity of paranthesis
+    if (!validParanthesis(display.innerText)) {
+      display.innerText = "SYNTAX ERROR";
+    }
     return;
   }
   // if (!operator && !["*", "/", "+", "-"].includes(event.target.value)) {
@@ -16,4 +24,25 @@ function Click(event) {
   //   display.innerText = val + event.target.value;
   // }
   display.innerText += event.target.value;
+}
+function validParanthesis(string) {
+  let arr = [];
+  let stack = [];
+  for (let i = 0; i < string.length; i++) {
+    const char = string.charAt(i);
+    if (char == "(" || char == ")") arr.push(char);
+  }
+  for (let c in arr) {
+    if (!stack || c == "(" || c == "{" || c == "[") {
+      stack.push(c);
+    } else if (c == ")" && stack.peek() == "(") {
+      stack.pop();
+    } else if (c == "}" && stack.peek() == "{") {
+      stack.pop();
+    } else if (c == "]" && stack.peek() == "[") {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
 }
