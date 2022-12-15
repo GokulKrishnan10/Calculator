c707ebc77736cecac69583edcd97e860c13912c6
GokulKrishnan10 <gokulkrish.elangovan10@gmail.com>
2022-12-15 19:51:58 +0530

    Made changes in JS file

--- calculator.js
+++ calculator.js
@@ -3,23 +3,8 @@ function Click(event) {
   console.log("BUtton clicked");
   const display = document.querySelector(".display");
   console.log(event.target.value);
-  if (["*", "/", "+", "-"].includes(event.target.value)) {
-    operator = event.target.value;
-  }
   const target = event.target.value;
-  if (target === "backspace") {
-    let text = display.innerText;
-    display.innerText = text.slice(0, -1);
-    return;
-  }
-  if (display.innerText === "SYNTAX ERROR") {
-    display.innerText = target;
-  }
-  if (!target) {
-    console.log("Clicked Clear");
-    display.innerText = "";
-    return;
-  }
+
   if (target === "=") {
     //Checking for validity of paranthesis
     if (!validParanthesis(display.innerText)) {
@@ -27,23 +12,49 @@ function Click(event) {
       return;
     }
   }
+
+  if (["*", "/", "+", "-"].includes(event.target.value)) {
+    operator = event.target.value;
+  }
+
+  if (target === "backspace") {
+    if (display.innerText === "SYNTAX ERROR") {
+      display.innerText = "";
+      return;
+    }
+    let text = display.innerText;
+    display.innerText = text.slice(0, -1);
+    return;
+  }
+
+  if (display.innerText === "SYNTAX ERROR") {
+    display.innerText = target;
+  }
+
+  if (!target) {
+    console.log("Clicked Clear");
+    display.innerText = "";
+    return;
+  }
+
   // if (!operator && !["*", "/", "+", "-"].includes(event.target.value)) {
   //   const val = display.innerText;
   //   display.innerText = val + event.target.value;
   // }
   display.innerText += event.target.value;
 }
+
 function validParanthesis(string) {
   if (!string) return true;
   let stack = [];
   for (let c of string) {
     if (!stack || c == "(" || c == "{" || c == "[") {
       stack.push(c);
-    } else if (c == ")" && stack.peek() == "(") {
+    } else if (c == ")" && stack[stack.length - 1] == "(") {
       stack.pop();
-    } else if (c == "}" && stack.peek() == "{") {
+    } else if (c == "}" && stack[stack.length - 1] == "{") {
       stack.pop();
-    } else if (c == "]" && stack.peek() == "[") {
+    } else if (c == "]" && stack[stack.length - 1] == "[") {
       stack.pop();
     } else {
       return false;
